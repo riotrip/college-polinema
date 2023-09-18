bcd1e9ae00ca82b93a56bfbfc3fae45110b7d1b2
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-09-18 14:49:15 +0700

    add

---
+++ minggu1/Latihan26.java
@@ -0,0 +1,6 @@
+public class Latihan26 {
+    public static void main(String[] args) {
+        System.out.print("Nama saya Rio Tri Prayogo");
+        // System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}   
