268aa2aca60d16940e085cdf75521808cf9bd26b
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-09-25 10:41:51 +0700

    pemilihan percobaan if else

---
+++ minggu5/PemilihanPercobaan126.java
@@ -0,0 +1,18 @@
+package minggu5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan126 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = input26.nextInt();
+        
+        if (angka %2 == 0) {
+            System.out.println("Angka "+angka+" bilangan genap");
+        } else {
+            System.out.println("Angka "+angka+" bilangan ganjil");
+        }
+    }
+}
