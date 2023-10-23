db8bb789bc2bee1cba261748f634aa94bd80408c
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-10-23 20:03:38 +0700

    array1 sorting asc to desc

--- minggu9/BubbleSortExample26.java
+++ minggu9/BubbleSortExample26.java
@@ -1,22 +1,22 @@
-package minggu9;
+    package minggu9;
 
-public class BubbleSortExample26 {
-    public static void main(String[] args) {
-        int[] intData = { 34, 18, 87, 72, 32, 54, 43 };
-        int temp = 0;
+    public class BubbleSortExample26 {
+        public static void main(String[] args) {
+            int[] intData = { 34, 18, 87, 72, 32, 54, 43 };
+            int temp = 0;
 
-        for (int i = 0; i < intData.length; i++) {
-            for (int j = 1; j < intData.length - i; j++) {
-                if (intData[j - 1] > intData[j]) {
-                    temp = intData[j];
-                    intData[j] = intData[j - 1];
-                    intData[j - 1] = temp;
+            for (int i = 0; i < intData.length; i++) {
+                for (int j = 1; j < intData.length - i; j++) {
+                    if (intData[j - 1] < intData[j]) {
+                        temp = intData[j];
+                        intData[j] = intData[j - 1];
+                        intData[j - 1] = temp;
+                    }
                 }
             }
-        }
-        System.out.println("Hasil pengurutan: ");
-        for (int i = 0; i < intData.length; i++) {
-            System.out.println(intData[i]);
+            System.out.println("Hasil pengurutan: ");
+            for (int i = 0; i < intData.length; i++) {
+                System.out.println(intData[i]);
+            }
         }
     }
-}
