82e40f1ad6434c24613c762c9b7fb4939050bdde
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-10-29 21:52:18 +0700

    array1 arrayRataNil lulus/tidak lulus

--- minggu9/ArrayRataNilai26.java
+++ minggu9/ArrayRataNilai26.java
@@ -6,18 +6,27 @@ public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = scan.nextInt();
+
+        int[] nilaiMhs = new int[jmlMhs];
+        double rataLulus, rataTdkLulus, totLulus = 0, totTdkLulus = 0;
+        int countLulus = 0, countTdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = scan.nextInt();
+            if (nilaiMhs[i] > 70) {
+                totLulus += nilaiMhs[i];
+                countLulus++;
+            } else {
+                totTdkLulus += nilaiMhs[i];
+                countTdkLulus++;
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        rataLulus = totLulus / countLulus;
+        rataTdkLulus = totTdkLulus / countTdkLulus;
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTdkLulus);
     }
 }
