66a955bd15e5afe579aa59b14ddac12254016d57
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-11-04 12:39:49 +0700

    array2 bioskopWithScanner

---
+++ minggu10/BioskopWithScanner26.java
@@ -0,0 +1,32 @@
+package minggu10;
+
+import java.util.Scanner;
+
+public class BioskopWithScanner26 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int baris, kolom;
+        String nama, next;
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan nama: ");
+            nama = scan.nextLine();
+            System.out.print("Masukkan baris: ");
+            baris = scan.nextInt();
+            System.out.print("Masukkan kolom: ");
+            kolom = scan.nextInt();
+            scan.nextLine();
+
+            penonton[baris - 1][kolom - 1] = nama;
+
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = scan.nextLine();
+            
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
