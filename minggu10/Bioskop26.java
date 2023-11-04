6f9ca7a29103a9e728c449ac9153f3f4dcd33dec
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-11-04 14:35:01 +0700

    menampilkan nama penonton pakai for loop

--- minggu10/Bioskop26.java
+++ minggu10/Bioskop26.java
@@ -14,9 +14,15 @@ public class Bioskop26 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        
+
         for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: "+barisPenonton.length);
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        System.out.println("Nama penonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
         }
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
