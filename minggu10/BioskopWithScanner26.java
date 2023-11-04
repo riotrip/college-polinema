901c7cc2709bf7878b4b8629123f6e783fc53ef1
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-11-04 16:26:10 +0700

    menambahkan opsi menu

--- minggu10/BioskopWithScanner26.java
+++ minggu10/BioskopWithScanner26.java
@@ -6,27 +6,48 @@ public class BioskopWithScanner26 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int baris, kolom;
+        int baris, kolom, pilihan;
         String nama, next;
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = scan.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = scan.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = scan.nextInt();
-            scan.nextLine();
+        do {
+            System.out.println("\nPilih Menu:");
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Data Penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu dalam (1/2/3): ");
+            pilihan = scan.nextInt();
+            switch (pilihan) {
+                case 1:
+                    boolean lanjut = true;
+                    while (lanjut) {
+                        scan.nextLine();
+                        System.out.print("Masukkan nama: ");
+                        nama = scan.nextLine();
+                        System.out.print("Masukkan baris: ");
+                        baris = scan.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = scan.nextInt();
+                        scan.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+                        penonton[baris - 1][kolom - 1] = nama;
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = scan.nextLine();
-            
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next = scan.next();
+
+                        lanjut = next.equalsIgnoreCase("y");
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < penonton.length; i++) {
+                        System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+                    }
+                    break;
+                case 3:
+                    return;
+                default:
+                    break;
             }
-        }
+        } while (pilihan >= 0);
     }
 }
