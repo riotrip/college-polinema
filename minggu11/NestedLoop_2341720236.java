fd8767d4f28e79f23119db99447c18b54eaaaec2
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-11-13 23:21:23 +0700

    tambah rata2 per kota

--- minggu11/NestedLoop_2341720236.java
+++ minggu11/NestedLoop_2341720236.java
@@ -6,6 +6,7 @@ public class NestedLoop_2341720236 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double[][] array = new double[5][7];
+        double[] rata2 = new double[5];
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("Kota ke-" + (i + 1));
@@ -16,14 +17,22 @@ public class NestedLoop_2341720236 {
             System.out.println();
         }
 
-        int i = 1;
+        for (int i = 0; i < rata2.length; i++) {
+            rata2[i] /= 7;
+        }
+
+        int k = 1;
         for (double[] kota : array) {
-            System.out.println("Kota ke-" + i);
+            System.out.println("Kota ke-" + k);
             for (double hari : kota) {
                 System.out.println(hari + " ");
             }
             System.out.println();
-            i++;
+            k++;
+        }
+
+        for (int i = 0; i < rata2.length; i++) {
+            System.out.println("Rata-rata kota ke-" + (i + 1) + ":" + rata2[i]);
         }
     }
 }
