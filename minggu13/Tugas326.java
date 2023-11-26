5a6a4dc0b34077d5c2b3e3f86d88e617ae9caf45
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-11-26 23:42:54 +0700

    tugas 3

--- minggu13/Tugas326.java
+++ minggu13/Tugas326.java
@@ -3,20 +3,23 @@ package minggu13;
 import java.util.Scanner;
 
 public class Tugas326 {
-    static void inputNilai(int[][] nilai, String[] nama) {
+    static void inputNamaDanNilai(int[][] nilai, String[] nama) {
         Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Nilai dari Mahasiswa " + nama[i]);
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println();
+            System.out.print("Masukkan nama Mahasiswa ke-" + (i + 1) + " : ");
+            nama[i] = scan.nextLine();
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai Minggu ke-" + (j + 1) + " : ");
                 nilai[i][j] = scan.nextInt();
             }
-            System.out.println();
+            scan.nextLine();
         }
     }
 
     static void tampilNilai(int[][] nilai, String[] nama) {
         int i = 0;
+        System.out.println();
         for (int a[] : nilai) {
             System.out.print("Nilai dari Mahasiswa " + nama[i] + "\t: ");
             for (int b : a) {
@@ -46,7 +49,9 @@ public class Tugas326 {
         for (int i = 0; i < nilai.length; i++) {
             for (int j = 0; j < nilai[i].length; j++) {
                 if (nilai[i][j] > nilaiMaks[0]) {
+                    // Nilai maks
                     nilaiMaks[0] = nilai[i][j];
+                    // Minggu nilai maks
                     nilaiMaks[1] = j + 1;
                 }
             }
@@ -54,17 +59,28 @@ public class Tugas326 {
     }
 
     public static void main(String[] args) {
-        int[][] nilaiMhs = new int[5][7];
-        String[] namaMhs = { "Sari", "Rina", "Yani", "Dwi", "Lusi" };
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah Mahasiswa\t: ");
+        int jmlMhs = scan.nextInt();
+        System.out.print("Masukkan jumlah nilai\t\t: ");
+        int jmlNilai = scan.nextInt();
+
+        int[][] nilaiMhs = new int[jmlMhs][jmlNilai];
+        String[] namaMhs = new String[jmlMhs];
         int[] nilaiTertinggi = new int[2];
 
-        inputNilai(nilaiMhs, namaMhs);
+        // Menginput nama dan nilai Mahasiswa
+        inputNamaDanNilai(nilaiMhs, namaMhs);
 
+        // Mengoutput seluruh nilai Mahasiswa
         tampilNilai(nilaiMhs, namaMhs);
 
+        // Output Nama Mahasiswa dengan nilai tertinggi
         String mhsNilaiTertinggi = cariMhsTertinggi(nilaiMhs, namaMhs);
         System.out.println("\nMahasiswa dengan nilai tertinggi adalah " + mhsNilaiTertinggi);
 
+        // Output nilai tertinggi dan minggu dari nilai tersebut
         cariNilaiMaks(nilaiMhs, nilaiTertinggi);
         System.out.println("Nilai tertinggi adalah " + nilaiTertinggi[0] + " di Minggu ke-" + nilaiTertinggi[1]);
 
