9f9022698c8e915fb4b6d7e53a847265d8e0f1fb
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-12-01 20:51:00 +0700

    add cetak deret perhitungan

--- minggu14/Percobaan2.java
+++ minggu14/Percobaan2.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
+            System.out.print(1 + " = ");
             return (1);
         } else {
+            System.out.print(x + "x");
             return (x * hitungPangkat(x, y - 1));
         }
     }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int bilangan, pangkat;
