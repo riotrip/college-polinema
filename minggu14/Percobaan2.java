d81b5c38d9ed03cbccaa3294d7b73fd3a0ab21b8
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2023-12-01 20:37:49 +0700

    fungsi rekursif hitung pangkat

---
+++ minggu14/Percobaan2.java
@@ -0,0 +1,24 @@
+package minggu14;
+
+import java.util.Scanner;
+
+public class Percobaan2 {
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x * hitungPangkat(x, y - 1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int bilangan, pangkat;
+
+        System.out.print("Bilangan yang dihitung: ");
+        bilangan = scan.nextInt();
+        System.out.print("Pangkat: ");
+        pangkat = scan.nextInt();
+
+        System.out.println(hitungPangkat(bilangan, pangkat));
+    }
+}
